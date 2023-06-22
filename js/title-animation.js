21046b6bd59a48bad99cf07297f5a60fd259fb5a
d10g0Te1xe1ra <diogoteixeira455@gmail.com>
2023-06-22 18:38:53 +0100

    main page

---
+++ js/title-animation.js
@@ -0,0 +1,13 @@
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 300,
+    delay: (el, i) => 200*i
+  });
